2f98cf404f58fdfd9bad823d04b21c581b49ef94
Jerry Yu <jyueatsthisorthat@gmail.com>
2026-09-05 12:16:19 -0400

    Add exhaustive complete-world shortlist DEV screen

---
+++ server/scripts/cwv_shortlist_screen.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""DEV exhaustive legal CWV shortlist paired screen."""
+from shengji.train.cwv_shortlist_screen import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
